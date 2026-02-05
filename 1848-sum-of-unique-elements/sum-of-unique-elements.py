dab97afd6f92e551dfee0058ce72036fdb23910e
Nikitha Matta <100593166+NikithaMatta@users.noreply.github.com>
2026-02-05 22:07:33 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (12.30%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.py
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.py
@@ -2,9 +2,9 @@ from collections import Counter
 class Solution:
     def sumOfUnique(self, nums: List[int]) -> int:
         count = Counter(nums)
-        sum = 0
+        sumi = 0
         for num, freq in count.items():
             if freq == 1:
-                sum += num
-        return sum
+                sumi += num
+        return sumi
         
