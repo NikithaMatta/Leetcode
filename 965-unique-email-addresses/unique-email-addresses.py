bf00d3b91d86b9c09ce3981072ba94308d049c42
Nikitha Matta <100593166+NikithaMatta@users.noreply.github.com>
2026-02-06 22:36:02 +0530

    Time: 4 ms (65.06%) | Memory: 19.5 MB (28.32%) - LeetSync

--- 965-unique-email-addresses/unique-email-addresses.py
+++ 965-unique-email-addresses/unique-email-addresses.py
@@ -3,8 +3,7 @@ class Solution:
         unique = set()
         for email in emails:
             local, domain = email.split('@')
-            local = local.split("+")[0]
-            local = local.replace(".", "")
+            local = local.split("+")[0].replace(".", "")
             unique.add(local+"@"+domain)
         return len(unique)
         
